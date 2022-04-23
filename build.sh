4a6275a2efa682389325ed4879b3d8f591787e4e
Heiko Panjas <heiko@panjas.com>
2022-04-23 15:00:08 +0200

    build: setup build script

--- build.sh
+++ build.sh
@@ -26,42 +26,81 @@
 #
 ################################################################################
 
-source scripts/BuildTools.sh
-
-TOOLS_DIRECTORY=`pwd`/tools
-BUILD_DIRECTORY=`pwd`/build
+BUILD_CLEAN=0
 BUILD_CONFIGURATION="Debug"
-CMAKE_GENERATOR="Unix Makefiles"
+BUILD_DIRECTORY="$(pwd)/build"
+BUILD_REBUILD=0
+BUILD_SHARED="ON"
+BUILD_THREADS=4
+
 CMAKE_EXTRA_ARGS=""
-
-if [ "$1" = "--help" ]; then
-  echo "Usage: build.sh [ --bootstrap | --rebuild | --clean | --cleanall ]"
-  exit 0
-elif [ "$1" = "--bootstrap" ]; then
-  RemoveDirectory $BUILD_DIRECTORY
-  RemoveDirectory $TOOLS_DIRECTORY
-  source ./bootstrap.sh
-  exit 0
-elif [ "$1" = "--cleanall" ]; then
-  RemoveDirectory $BUILD_DIRECTORY
-  RemoveDirectory $TOOLS_DIRECTORY
-  exit 0
-elif [ "$1" = "--clean" ]; then
-  RemoveDirectory $BUILD_DIRECTORY
-  exit 0
-elif [ "$1" == "--rebuild" ]; then
-  CMAKE_EXTRA_ARGS="--clean-first"
-elif [ "$1" == "--release" ]; then
-  BUILD_CONFIGURATION=Release
-fi
-
-CMAKE_INSTALL_DIRECTORY=$TOOLS_DIRECTORY/cmake
+CMAKE_GENERATOR="Unix Makefiles"
+# CMAKE_GENERATOR="Ninja Multi-Config"
 CMAKE_INSTALL_FOUND=0
-CMAKE_REQUIRED_VERSION="3.12.0"
+CMAKE_INSTALL_PATH=cmake
+CMAKE_REQUIRED_VERSION="3.19.0"
 
-echo "Looking for CMake $CMAKE_REQUIRED_VERSION. Checking system install..."
+source "scripts/buildtools.sh"
+
+for arg in "$@"
+do
+case $arg in
+    --bootstrap)
+    BUILD_BOOTSTRAP=1
+    shift # past argument
+    ;;
+    --clean)
+    BUILD_CLEAN=1
+    shift # past argument
+    ;;
+    -c=*|--config=*)
+    BUILD_CONFIGURATION="${arg#*=}"
+    shift # past argument
+    ;;
+    -n|--no-shared)
+    BUILD_SHARED="OFF"
+    shift # past argument
+    ;;
+    --rebuild)
+    CMAKE_EXTRA_ARGS="--clean-first"
+    shift # past argument
+    ;;
+    -r|--release)
+    BUILD_CONFIGURATION="Release"
+    shift # past argument
+    ;;
+    "-h"|"--help")
+    echo "Usage: build.sh [options]"
+    echo ""
+    echo "Options:"
+    echo "  --clean                                     Completely remove build output"
+    echo "  -c=<BUILD_CONFIG>|--config=<BUILD_CONFIG>   Specify build configuration (default = 'Debug')"
+    echo "  -n|--no-shared                              Don't build shared libraries"
+    echo "  --rebuild                                   Remove intermediate files before build"
+    echo "  --release                                   Don't include debug symbols, overrides '-c|--config'"
+    echo ""
+    exit 0
+    shift # past argument
+    ;;
+    *)    # unknown option
+    echo "Unknown argument $arg"
+    exit 1
+    ;;
+esac
+done
+
+if [ $BUILD_CLEAN -eq 1 ]; then
+  if [ -d "$BUILD_DIRECTORY" ]; then
+    rm -rf "$BUILD_DIRECTORY"
+  fi
+  if [ -d "$BUILD_TARGET_DIRECTORY" ]; then
+    rm -rf "$BUILD_TARGET_DIRECTORY"
+  fi
+  exit 1
+fi
+
+echo "Looking for CMake $CMAKE_REQUIRED_VERSION..."
 if [ $CMAKE_INSTALL_FOUND -eq 0 ]; then
-  CMAKE_INSTALL_PATH=cmake
   if [ -x "$(command -v $CMAKE_INSTALL_PATH)" ]; then
     CMAKE_CURRENT_VERSION=`$CMAKE_INSTALL_PATH --version | sort -V | tail -n 1 | awk -v n=3 '{print $n}'`
     echo "Found CMake version $CMAKE_CURRENT_VERSION."
@@ -73,35 +112,30 @@ if [ $CMAKE_INSTALL_FOUND -eq 0 ]; then
 fi
 
 if [ $CMAKE_INSTALL_FOUND -eq 0 ]; then
-  echo "CMake $CMAKE_REQUIRED_VERSION does not seem to be installed on this system. Checking local install..."
-  CMAKE_INSTALL_PATH=$CMAKE_INSTALL_DIRECTORY/bin/cmake
-  if [ -x "$(command -v $CMAKE_INSTALL_PATH)" ]; then
-    CMAKE_CURRENT_VERSION=`$CMAKE_INSTALL_PATH --version | sort -V | tail -n 1 | awk -v n=3 '{print $n}'`
-    echo "Found CMake version $CMAKE_CURRENT_VERSION."
-    CompareVersions $CMAKE_CURRENT_VERSION $CMAKE_REQUIRED_VERSION
-    if [ ! $? -eq 2 ]; then
-      CMAKE_INSTALL_FOUND=1
-      export PATH=$CMAKE_INSTALL_DIRECTORY/bin:$PATH
+  echo "CMake not found. Please install at least CMake version $CMAKE_REQUIRED_VERSION."
+  exit 1
+fi
+
+if [ ! -d "$BUILD_DIRECTORY" ]; then
+    echo "Configuring projects using $CMAKE_GENERATOR..."
+    cmake -B"$BUILD_DIRECTORY" -G"$CMAKE_GENERATOR" -Wno-dev --no-warn-unused-cli -DBUILD_SHARED_LIBS="$BUILD_SHARED" -DCMAKE_BUILD_TYPE="$BUILD_CONFIGURATION"
+    if [ $? -ne 0 ]; then
+      echo "Failed to configure projects."
+      exit -1
     fi
-  fi
+    echo "Done."
 fi
 
-if [ $CMAKE_INSTALL_FOUND -ne 0 ]; then
-  echo "Configuring projects using $CMAKE_GENERATOR..."
-  cmake -B"$BUILD_DIRECTORY" -G"$CMAKE_GENERATOR" -Wno-dev -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=$BUILD_CONFIGURATION
-  if [ $? -ne 0 ]; then
-    echo "Failed to configure projects."
-    exit -1
-  fi
-  echo "Done."
-
-  echo "Building projects using $CMAKE_GENERATOR..."
-  cmake --build "$BUILD_DIRECTORY" $CMAKE_EXTRA_ARGS --config $BUILD_CONFIGURATION -j
-  if [ $? -ne 0 ]; then
-    echo "Failed to build projects."
-    exit -1
-  fi
-  echo "Done."
+if [ -x "$(command -v nproc)" ]; then
+  BUILD_THREADS=$(nproc)
 fi
+
+echo "Building projects using $CMAKE_GENERATOR..."
+cmake --build "$BUILD_DIRECTORY" $CMAKE_EXTRA_ARGS --config "$BUILD_CONFIGURATION" -j "$BUILD_THREADS"
+if [ $? -ne 0 ]; then
+  echo "Failed to build projects."
+  exit -1
+fi
+echo "Done."
 
 exit 0
