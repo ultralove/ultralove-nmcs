cbaf91e5c817a7a75c2554d44531540b8ef8f970
Heiko Panjas <heiko@panjas.com>
2023-05-11 13:41:08 +0200

    feat(core): Remove duplicate status code

--- include/nmcs/status.h
+++ include/nmcs/status.h
@@ -44,8 +44,7 @@ typedef uint32_t NmcsStatus;
 #define NMCS_STATUS_NOT_IMPLEMENTED                                 NMCS_MAKE_STATUS_CODE(NMCS_SEVERITY_ERROR, 0, 0x00000004)
 #define NMCS_STATUS_ALREADY_REGISTERED                              NMCS_MAKE_STATUS_CODE(NMCS_SEVERITY_ERROR, 0, 0x00000005)
 #define NMCS_STATUS_OUT_OF_MEMORY                                   NMCS_MAKE_STATUS_CODE(NMCS_SEVERITY_ERROR, 0, 0x00000006)
-#define NMCS_STATUS_NOT_FOUND                                       NMCS_MAKE_STATUS_CODE(NMCS_SEVERITY_ERROR, 0, 0x00000007)
-#define NMCS_STATUS_ALREADY_EXISTS                                  NMCS_MAKE_STATUS_CODE(NMCS_SEVERITY_ERROR, 0, 0x00000008)
+#define NMCS_STATUS_ALREADY_EXISTS                                  NMCS_MAKE_STATUS_CODE(NMCS_SEVERITY_ERROR, 0, 0x00000007)
 
 #define NMCS_SUCCEEDED(__status__)                                  (NMCS_STATUS_SUCCESS == (__status__))
 #define NMCS_FAILED(__status__)                                     (NMCS_STATUS_SUCCESS != (__status__))
