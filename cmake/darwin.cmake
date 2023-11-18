3327b0d557dc12ea6d6094566e314166149090d6
Heiko Panjas <heiko@panjas.com>
2023-11-18 22:51:35 +0100

    feat(build): Fix macos build, remove grpc

--- cmake/darwin.cmake
+++ cmake/darwin.cmake
@@ -29,45 +29,41 @@ include(FetchContent)
 set(FETCHCONTENT_QUIET   ON)
 set(FETCHCONTENT_VERBOSE OFF)
 
-FetchContent_Declare(cli11
+FetchContent_Declare(libcli11
   GIT_REPOSITORY "https://github.com/CLIUtils/CLI11.git"
   GIT_SHALLOW  ON
   GIT_PROGRESS OFF
   GIT_TAG ${LIBCLI11_VERSION_TAG}
 )
-FetchContent_GetProperties(cli11)
-if(NOT cli11_POPULATED)
-  FetchContent_Populate(cli11)
-  set(LIBCLI11_INCLUDE_PATH  ${cli11_SOURCE_DIR}/include)
-  set(BUILD_SHARED_LIBS_SAVE ${BUILD_SHARED_LIBS})
-  set(BUILD_SHARED_LIBS      OFF)
-  set(CLI11_SINGLE_FILE      OFF)
-  set(CLI11_BUILD_DOCS       OFF)
-  set(CLI11_BUILD_TESTS      OFF)
-  set(CLI11_BUILD_EXAMPLES   OFF)
-  add_subdirectory(${cli11_SOURCE_DIR} ${cli11_BINARY_DIR})
-  set(BUILD_SHARED_LIBS      ${BUILD_SHARED_LIBS_SAVE})
+FetchContent_GetProperties(libcli11)
+if(NOT libcli11_POPULATED)
+  FetchContent_Populate(libcli11)
+  set(LIBCLI11_INCLUDE_PATH ${libcli11_SOURCE_DIR}/include)
+  set(BUILD_SHARED_LIBS     OFF CACHE INTERNAL "")
+  set(CLI11_SINGLE_FILE     OFF CACHE INTERNAL "")
+  set(CLI11_BUILD_DOCS      OFF CACHE INTERNAL "")
+  set(CLI11_BUILD_TESTS     OFF CACHE INTERNAL "")
+  set(CLI11_BUILD_EXAMPLES  OFF CACHE INTERNAL "")
+  add_subdirectory(${libcli11_SOURCE_DIR} ${libcli11_BINARY_DIR})
 endif()
 
-# set(LIBZ_LIBRARY libz)
-# FetchContent_Declare(libz
-#   GIT_REPOSITORY https://github.com/madler/zlib.git
-#   GIT_SHALLOW  ON
-#   GIT_PROGRESS OFF
-#   GIT_TAG ${LIBZ_VERSION_TAG}
-# )
-# FetchContent_GetProperties(libz)
-# if(NOT libz_POPULATED)
-#   FetchContent_Populate(libz)
-#   set(LIBZ_INCLUDE_PATH      ${libz_BINARY_DIR} ${libz_SOURCE_DIR} ${libz_SOURCE_DIR}/contrib/minizip)
-#   set(LIBZ_LIBRARY_PATH      ${libz_BINARY_DIR}/libz.a)
-#   set(LIBZ_SOURCE_PATH       ${libz_SOURCE_DIR})
-#   set(BUILD_SHARED_LIBS_SAVE ${BUILD_SHARED_LIBS})
-#   set(BUILD_SHARED_LIBS      OFF)
-#   set(SKIP_INSTALL_ALL       ON)
-#   add_subdirectory(${libz_SOURCE_DIR} ${libz_BINARY_DIR})
-#   set(BUILD_SHARED_LIBS      ${BUILD_SHARED_LIBS_SAVE})
-# endif()
+set(LIBZ_LIBRARY libz)
+FetchContent_Declare(libz
+  GIT_REPOSITORY https://github.com/madler/zlib.git
+  GIT_SHALLOW  ON
+  GIT_PROGRESS OFF
+  GIT_TAG ${LIBZ_VERSION_TAG}
+)
+FetchContent_GetProperties(libz)
+if(NOT libz_POPULATED)
+  FetchContent_Populate(libz)
+  set(LIBZ_INCLUDE_PATH ${libz_BINARY_DIR} ${libz_SOURCE_DIR} ${libz_SOURCE_DIR}/contrib/minizip)
+  set(LIBZ_LIBRARY_PATH ${libz_BINARY_DIR}/libz.a)
+  set(LIBZ_SOURCE_PATH  ${libz_SOURCE_DIR})
+  set(BUILD_SHARED_LIBS OFF CACHE INTERNAL "")
+  set(SKIP_INSTALL_ALL  ON CACHE INTERNAL "")
+  add_subdirectory(${libz_SOURCE_DIR} ${libz_BINARY_DIR})
+endif()
 
 set(LIBCURL_LIBRARY libcurl)
 FetchContent_Declare(libcurl
@@ -85,24 +81,22 @@ if(NOT libcurl_POPULATED)
   else()
     set(LIBCURL_LIBRARY_PATH ${libcurl_BINARY_DIR}/lib/libcurl.a)
   endif()
-  set(BUILD_SHARED_LIBS_SAVE ${BUILD_SHARED_LIBS})
-  set(BUILD_SHARED_LIBS      OFF)
-  set(BUILD_CURL_EXE         OFF)
-  set(BUILD_BINDINGS         OFF)
-  set(BUILD_TESTING          OFF)
-  set(CURL_ENABLE_SSL        ON)
-  set(CURL_USE_SECTRANSP     ON)
-  set(CURL_CA_PATH           none)
-  set(CURL_DISABLE_LDAP      ON)
-  set(CURL_DISABLE_LDAPS     ON)
-  set(CURL_DISABLE_NTLM      ON)
-  set(CURL_DISABLE_ALTSVC    ON)
-  set(ENABLE_IPV6            ON)
-  set(ENABLE_MANUAL          OFF)
-  set(HTTP_ONLY              ON)
-  set(USE_UNIX_SOCKETS       OFF)
+  set(BUILD_SHARED_LIBS   OFF CACHE INTERNAL "")
+  set(BUILD_CURL_EXE      OFF CACHE INTERNAL "")
+  set(BUILD_BINDINGS      OFF CACHE INTERNAL "")
+  set(BUILD_TESTING       OFF CACHE INTERNAL "")
+  set(ENABLE_MANUAL       OFF CACHE INTERNAL "")
+  set(USE_UNIX_SOCKETS    OFF CACHE INTERNAL "")
+  set(CURL_ENABLE_SSL     ON CACHE INTERNAL "")
+  set(CURL_USE_SECTRANSP  ON CACHE INTERNAL "")
+  set(CURL_DISABLE_LDAP   ON CACHE INTERNAL "")
+  set(CURL_DISABLE_LDAPS  ON CACHE INTERNAL "")
+  set(CURL_DISABLE_NTLM   ON CACHE INTERNAL "")
+  set(CURL_DISABLE_ALTSVC ON CACHE INTERNAL "")
+  set(ENABLE_IPV6         ON CACHE INTERNAL "")
+  set(HTTP_ONLY           ON CACHE INTERNAL "")
+  set(CURL_CA_PATH        none CACHE INTERNAL "")
   add_subdirectory(${libcurl_SOURCE_DIR} ${libcurl_BINARY_DIR})
-  set(BUILD_SHARED_LIBS      ${BUILD_SHARED_LIBS_SAVE})
 endif()
 
 FetchContent_Declare(libxml2
@@ -114,104 +108,93 @@ FetchContent_Declare(libxml2
 FetchContent_GetProperties(libxml2)
 if(NOT libxml2_POPULATED)
   FetchContent_Populate(libxml2)
-  set(LIBXML2_INCLUDE_PATH   ${libxml2_SOURCE_DIR})
-  set(LIBXML2_LIBRARY_PATH   ${libxml2_BINARY_DIR}/libxml2.a)
-  set(BUILD_SHARED_LIBS_SAVE ${BUILD_SHARED_LIBS})
-  set(BUILD_SHARED_LIBS      OFF)
-  set(LIBXML2_WITH_LEGACY    OFF)
-  set(LIBXML2_WITH_LZMA      OFF)
-  set(LIBXML2_WITH_PROGRAMS  OFF)
-  set(LIBXML2_WITH_PYTHON    OFF)
-  set(LIBXML2_WITH_TESTS     OFF)
-  set(LIBXML2_WITH_DEBUG     OFF)
+  set(LIBXML2_INCLUDE_PATH  ${libxml2_SOURCE_DIR})
+  set(LIBXML2_LIBRARY_PATH  ${libxml2_BINARY_DIR}/libxml2.a)
+  set(BUILD_SHARED_LIBS     OFF CACHE INTERNAL "")
+  set(LIBXML2_WITH_LEGACY   OFF CACHE INTERNAL "")
+  set(LIBXML2_WITH_LZMA     OFF CACHE INTERNAL "")
+  set(LIBXML2_WITH_PROGRAMS OFF CACHE INTERNAL "")
+  set(LIBXML2_WITH_PYTHON   OFF CACHE INTERNAL "")
+  set(LIBXML2_WITH_TESTS    OFF CACHE INTERNAL "")
+  set(LIBXML2_WITH_DEBUG    OFF CACHE INTERNAL "")
   add_subdirectory(${libxml2_SOURCE_DIR} ${libxml2_BINARY_DIR})
-  set(BUILD_SHARED_LIBS      ${BUILD_SHARED_LIBS_SAVE})
 endif()
 
-FetchContent_Declare(simdjson
-  GIT_REPOSITORY https://github.com/simdjson/simdjson.git
-  GIT_SHALLOW  ON
-  GIT_PROGRESS OFF
-  GIT_TAG ${LIBSIMDJSON_VERSION_TAG}
-)
-FetchContent_GetProperties(simdjson)
-if(NOT simdjson_POPULATED)
-  FetchContent_Populate(simdjson)
-  set(LIBSIMDJSON_INCLUDE_PATH        ${simdjson_SOURCE_DIR}/include)
-  set(LIBSIMDJSON_LIBRARY_PATH        ${simdjson_BINARY_DIR}/libsimdjson.a)
-  set(BUILD_SHARED_LIBS_SAVE          ${BUILD_SHARED_LIBS})
-  set(BUILD_SHARED_LIBS               OFF)
-  set(SIMDJSON_DEVELOPER_MODE         OFF)
-  set(SIMDJSON_DISABLE_DEPRECATED_API OFF)
-  set(SIMDJSON_ALLOW_DOWNLOADS        OFF)
-  set(SIMDJSON_ENABLE_FUZZING         OFF)
-  add_subdirectory(${simdjson_SOURCE_DIR} ${simdjson_BINARY_DIR})
-  set(BUILD_SHARED_LIBS               ${BUILD_SHARED_LIBS_SAVE})
-endif()
-
-FetchContent_Declare(spdlog
-  GIT_REPOSITORY https://github.com/gabime/spdlog.git
-  GIT_SHALLOW  ON
-  GIT_PROGRESS OFF
-  GIT_TAG ${LIBSPDLOG_VERSION_TAG}
-)
-FetchContent_GetProperties(spdlog)
-if(NOT spdlog_POPULATED)
-  FetchContent_Populate(spdlog)
-  set(LIBSPDLOG_INCLUDE_PATH ${spdlog_SOURCE_DIR}/include)
-  if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-    set(LIBSPDLOG_LIBRARY_PATH ${spdlog_BINARY_DIR}/libspdlogd.a)
-  else()
-    set(LIBSPDLOG_LIBRARY_PATH ${spdlog_BINARY_DIR}/libspdlog.a)
-  endif()
-  set(BUILD_SHARED_LIBS_SAVE ${BUILD_SHARED_LIBS})
-  set(BUILD_SHARED_LIBS      OFF)
-  set(SPDLOG_BUILD_EXAMPLE   OFF)
-  add_subdirectory(${spdlog_SOURCE_DIR} ${spdlog_BINARY_DIR})
-  set(BUILD_SHARED_LIBS      ${BUILD_SHARED_LIBS_SAVE})
-endif()
-
-FetchContent_Declare(simdutf
+FetchContent_Declare(libsimdutf
   GIT_REPOSITORY https://github.com/simdutf/simdutf.git
   GIT_SHALLOW  ON
   GIT_PROGRESS OFF
   GIT_TAG ${LIBSIMDUTF_VERSION_TAG}
 )
-FetchContent_GetProperties(simdutf)
-if(NOT simdutf_POPULATED)
-  FetchContent_Populate(simdutf)
-  set(LIBSIMDUTF_INCLUDE_PATH ${simdutf_SOURCE_DIR}/include)
-  set(LIBSIMDUTF_LIBRARY_PATH ${simdutf_BINARY_DIR}/src/libsimdutf.a)
-  set(BUILD_SHARED_LIBS_SAVE  ${BUILD_SHARED_LIBS})
-  set(BUILD_SHARED_LIBS       OFF)
-  set(SIMDUTF_TESTS           OFF)
-  set(SIMDUTF_BENCHMARKS      OFF)
-  set(SIMDUTF_TOOLS           OFF)
-  set(SIMDUTF_ICONV           OFF)
-  add_subdirectory(${simdutf_SOURCE_DIR} ${simdutf_BINARY_DIR})
-  set(BUILD_SHARED_LIBS       ${BUILD_SHARED_LIBS_SAVE})
+FetchContent_GetProperties(libsimdutf)
+if(NOT libsimdutf_POPULATED)
+  FetchContent_Populate(libsimdutf)
+  set(LIBSIMDUTF_INCLUDE_PATH ${libsimdutf_SOURCE_DIR}/include)
+  set(LIBSIMDUTF_LIBRARY_PATH ${libsimdutf_BINARY_DIR}/src/libsimdutf.a)
+  set(BUILD_SHARED_LIBS       OFF CACHE INTERNAL "")
+  set(SIMDUTF_TESTS           OFF CACHE INTERNAL "")
+  set(SIMDUTF_BENCHMARKS      OFF CACHE INTERNAL "")
+  set(SIMDUTF_TOOLS           OFF CACHE INTERNAL "")
+  set(SIMDUTF_ICONV           OFF CACHE INTERNAL "")
+  add_subdirectory(${libsimdutf_SOURCE_DIR} ${libsimdutf_BINARY_DIR})
 endif()
 
-FetchContent_Declare(whisper.cpp
+FetchContent_Declare(libsimdjson
+  GIT_REPOSITORY https://github.com/simdjson/simdjson.git
+  GIT_SHALLOW  ON
+  GIT_PROGRESS OFF
+  GIT_TAG ${LIBSIMDJSON_VERSION_TAG}
+)
+FetchContent_GetProperties(libsimdjson)
+if(NOT libsimdjson_POPULATED)
+  FetchContent_Populate(libsimdjson)
+  set(LIBSIMDJSON_INCLUDE_PATH        ${libsimdjson_SOURCE_DIR}/include)
+  set(LIBSIMDJSON_LIBRARY_PATH        ${libsimdjson_BINARY_DIR}/libsimdjson.a)
+  set(BUILD_SHARED_LIBS               OFF CACHE INTERNAL "")
+  set(SIMDJSON_DEVELOPER_MODE         OFF CACHE INTERNAL "")
+  set(SIMDJSON_DISABLE_DEPRECATED_API OFF CACHE INTERNAL "")
+  set(SIMDJSON_ALLOW_DOWNLOADS        OFF CACHE INTERNAL "")
+  set(SIMDJSON_ENABLE_FUZZING         OFF CACHE INTERNAL "")
+  add_subdirectory(${libsimdjson_SOURCE_DIR} ${libsimdjson_BINARY_DIR})
+endif()
+
+FetchContent_Declare(libspdlog
+  GIT_REPOSITORY https://github.com/gabime/spdlog.git
+  GIT_SHALLOW  ON
+  GIT_PROGRESS OFF
+  GIT_TAG ${LIBSPDLOG_VERSION_TAG}
+)
+FetchContent_GetProperties(libspdlog)
+if(NOT libspdlog_POPULATED)
+  FetchContent_Populate(libspdlog)
+  set(LIBSPDLOG_INCLUDE_PATH ${libspdlog_SOURCE_DIR}/include)
+  if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+    set(LIBSPDLOG_LIBRARY_PATH ${libspdlog_BINARY_DIR}/libspdlogd.a)
+  else()
+    set(LIBSPDLOG_LIBRARY_PATH ${libspdlog_BINARY_DIR}/libspdlog.a)
+  endif()
+  set(BUILD_SHARED_LIBS    OFF CACHE INTERNAL "")
+  set(SPDLOG_BUILD_EXAMPLE OFF CACHE INTERNAL "")
+  add_subdirectory(${libspdlog_SOURCE_DIR} ${libspdlog_BINARY_DIR})
+endif()
+
+FetchContent_Declare(libwhisper
   GIT_REPOSITORY https://github.com/ggerganov/whisper.cpp.git
   GIT_SHALLOW  ON
   GIT_PROGRESS OFF
   GIT_TAG ${LIBWHISPER_VERSION_TAG}
 )
-FetchContent_GetProperties(whisper.cpp)
-if(NOT whisper.cpp_POPULATED)
-  FetchContent_Populate(whisper.cpp)
-  set(LIBWHISPER_INCLUDE_PATH        ${whisper.cpp_SOURCE_DIR})
-  set(LIBWHISPER_LIBRARY_PATH        ${whisper.cpp_BINARY_DIR}/libwhisper.a)
-  set(BUILD_SHARED_LIBS_SAVE         ${BUILD_SHARED_LIBS})
-  set(BUILD_SHARED_LIBS              OFF)
-  set(WHISPER_ALL_WARNINGS           OFF)
-  set(WHISPER_ALL_WARNINGS_3RD_PARTY OFF)
-  set(WHISPER_BUILD_TESTS            OFF)
-  set(WHISPER_BUILD_EXAMPLES         OFF)
-  set(WHISPER_PERF                   OFF)
-  add_subdirectory(${whisper.cpp_SOURCE_DIR} ${whisper.cpp_BINARY_DIR})
-  set(BUILD_SHARED_LIBS              ${BUILD_SHARED_LIBS_SAVE})
+FetchContent_GetProperties(libwhisper)
+if(NOT libwhisper_POPULATED)
+  FetchContent_Populate(libwhisper)
+  set(LIBWHISPER_INCLUDE_PATH        ${libwhisper_SOURCE_DIR})
+  set(LIBWHISPER_LIBRARY_PATH        ${libwhisper_BINARY_DIR}/libwhisper.a)
+  set(WHISPER_ALL_WARNINGS           OFF CACHE INTERNAL "")
+  set(WHISPER_ALL_WARNINGS_3RD_PARTY OFF CACHE INTERNAL "")
+  set(WHISPER_BUILD_TESTS            OFF CACHE INTERNAL "")
+  set(WHISPER_BUILD_EXAMPLES         OFF CACHE INTERNAL "")
+  set(WHISPER_PERF                   OFF CACHE INTERNAL "")
+  add_subdirectory(${libwhisper_SOURCE_DIR} ${libwhisper_BINARY_DIR})
 endif()
 
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin")
